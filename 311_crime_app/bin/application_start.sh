161bdb16de87e5893852fb0b3b5ba73b09ca8ea8
Marc Richardson <mtrichardson@uchicago.edu>
2020-12-08 19:22:12 -0700

    add new port to start application sh

--- 311_crime_app/bin/application_start.sh
+++ 311_crime_app/bin/application_start.sh
@@ -1,2 +1,2 @@
 cd /home/ec2-user/mtrichardson/311_crime_app
-forever --id "mtrichardson_311_crime_app" start app.js 3008 ip-172-31-11-144.us-east-2.compute.internal 8070
+forever --id "mtrichardson_311_crime_app" start app.js 3303 ip-172-31-11-144.us-east-2.compute.internal 8070
